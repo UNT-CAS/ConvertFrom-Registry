dbe9de8cfecf76fee0256c7defab3f3507f998e6
Raymond Piller <VertigoRay@users.noreply.github.com>
2016-02-01 00:32:42 -0500

    Update ConvertFrom-Registry.ps1

--- ConvertFrom-Registry.ps1
+++ ConvertFrom-Registry.ps1
@@ -30,23 +30,7 @@ PS > $TestApp.Server = 'testapp.its.cas.unt.edu'
 PS > $TestApp.Format = 'html'
 PS > (ConvertFrom-Registry -Key (Resolve-Path 'HKLM:\SOFTWARE\TestApp\' -ErrorAction Stop) -Recurse -Ignore @('Parameters')).GetEnumerator() | %{ $TestApp.($_.Name) = $_.Value }
 .NOTES
-Author: Raymond Piller (VertigoRay)
-
-Copyright 2016 University of North Texas,
-               College of Arts & Sciences,
-               IT Services
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Author: Raymond Piller ([VertigoRay](https://github.com/VertigoRay))
 .LINK
 http://github.com/UNT-CAS-ITS/ConvertFrom-Registry
 #>
